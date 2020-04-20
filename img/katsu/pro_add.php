4ade36666a984b7bf31720df208c9d715cda364c
img-onioniserver <63284293+img-onioniserver@users.noreply.github.com>
2020-04-20 19:55:23 +0900

    Add files via upload

---
+++ img/katsu/pro_add.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Upload</title>
+</head>
+<body>
+画像を選んでください。<br />
+<input type="file" name="gazou" style="width:400px"><br />
+<br />
+<input type="button" onclick="history.back()" value="戻る">
+<input type="submit" value="ＯＫ">
+</form>
+
+</body>
+</html>
